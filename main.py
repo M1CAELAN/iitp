0f20424e049c76fcbfad7830e5102e72ac16663b
M1CAELAN <michaelklinkov@gmail.com>
2023-09-13 00:45:34 +0300

    second commit

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 from random import randint
-print("вы хотите бросить д 20?")
+print("вы хотите бросить д20, д10, д8, д6 ил д4?")
 a = input()
-print(randint(1, 20))
+if a == "д20":
+    print(randint(1, 20))
+elif a == "д10":
+    print(randint(1, 10))
+elif a == "д8":
+    print(randint(1, 8))
+elif a == "д6":
+    print(randint(1, 6))
+elif a == "д4":
+    print(randint(1, 4))
+else:
+    print("правильно вводи, а не это ваше")
